189d6a033f78dedddde74cec34ec424d62e05a7c
Goobus <huand2464@wrdsb.ca>
2026-01-23 09:18:47 -0500

    v0.0.1: Modify set_square to accept a Piece parameter

--- Versions/position.h
+++ Versions/position.h
@@ -29,7 +29,7 @@ struct Position
     // idk i thnk this is what you do
     void set_start_pos();
     void update_occupancies();
-    void set_square();
+    void set_square(Piece piece);
     void parse_fen(const std::string& fen);
     Bitboard get_bitboard (Piece piece) const;
     
